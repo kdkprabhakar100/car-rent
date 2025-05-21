3528e1e463d03c31da1e2e3193b10e1adce5ea0f
kdkprabhakar <kdkprabhakar100@gmail.com>
2025-05-21 22:18:35 +0545

    BEM-responsive

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ function toggleIcons() {
   const icons = document.getElementById("hiddenIcons");
   icons.classList.toggle("active");
 }
+
+// Close the icons if clicked outside
+document.addEventListener("click", function (event) {
+  const icons = document.getElementById("hiddenIcons");
+  const profile = document.querySelector(".navbar__user-profile");
+
+  // If icons are open and the clicked target is not inside icons or profile
+  if (
+    icons.classList.contains("active") &&
+    !icons.contains(event.target) &&
+    !profile.contains(event.target)
+  ) {
+    icons.classList.remove("active");
+  }
+});
