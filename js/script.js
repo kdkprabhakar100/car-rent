b0f9057f74f9b1957dacd75b6eb658814dcfc72a
kdkprabhakar <kdkprabhakar100@gmail.com>
2025-05-23 09:51:18 +0545

    hero-section

--- js/script.js
+++ js/script.js
@@ -3,17 +3,11 @@ function toggleIcons() {
   icons.classList.toggle("active");
 }
 
-// Close the icons if clicked outside
 document.addEventListener("click", function (event) {
   const icons = document.getElementById("hiddenIcons");
   const profile = document.querySelector(".navbar__user-profile");
 
-  // If icons are open and the clicked target is not inside icons or profile
-  if (
-    icons.classList.contains("active") &&
-    !icons.contains(event.target) &&
-    !profile.contains(event.target)
-  ) {
+  if (!profile.contains(event.target) && !icons.contains(event.target)) {
     icons.classList.remove("active");
   }
 });
